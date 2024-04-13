569b1f727a257cf6dd72cd20bbc033bcdfc1298b
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2024-04-13 13:53:06 +0500

    new addition

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages ,setup
+# from typing import List
+
+setup (
+name= 'sensor',
+version= '0.0.1',
+author= 'Abdul Qadir',
+author_email= 'aq452831@gmail.com',
+packages= find_packages(),
+)
