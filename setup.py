aacafd7207fb3918349d803e9a5e284d009523da
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2024-04-23 19:59:09 +0500

    after adding e

--- setup.py
+++ setup.py
@@ -1,44 +1,30 @@
-# from setuptools import find_packages ,setup
-# # from typing import list
-
-# def get_requirements()->list[str]:
-    
-#     req_list = list[str] = []
-
-#     return req_list
-
-
-
-
-
-# setup(
-# name= 'sensor',
-# version= '0.0.1',
-# author= 'Abdul Qadir',
-# author_email= 'aq452831@gmail.com',
-# packages= find_packages(),
-# # install_requires=['pymongo'] #avoiding list for package installation
-# install_requires=get_requirements() #function calling
-
-# )
-
-
-
-
-
-
-
+# Importing necessary functions from setuptools package
 from setuptools import find_packages, setup
 
+# Function to retrieve project dependencies
 def get_requirements() -> list[str]:
+    # Initialize an empty list to store requirements
     req_list: list[str] = []
+    # Return the list of requirements
     return req_list
 
+# Configuration of the project setup
 setup(
+    # Name of the project
     name='sensor',
+
+    # Version of the project
     version='0.0.1',
+
+    # Author of the project
     author='Abdul Qadir',
+
+    # Email address of the project author
     author_email='aq452831@gmail.com',
+
+    # Automatically find all packages and sub-packages in the project
     packages=find_packages(),
+
+    # Retrieve project dependencies using the get_requirements function
     install_requires=get_requirements()
 )
